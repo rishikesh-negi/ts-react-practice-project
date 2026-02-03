b46fd6d8a3cc4e32008cd4be2063111bb31f9ade
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-03 16:21:08 +0530

    Single quotes replaced with double quotes by Prettier code formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import HomePage from './pages/Home.tsx';
-import SessionsPage from './pages/Sessions.tsx';
-import SessionPage from './pages/Session.tsx';
-import Root from './pages/Root.tsx';
+import HomePage from "./pages/Home.tsx";
+import SessionsPage from "./pages/Sessions.tsx";
+import SessionPage from "./pages/Session.tsx";
+import Root from "./pages/Root.tsx";
 
 const Router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     children: [
       {
         index: true,
         element: <HomePage />,
       },
-      { path: 'sessions', element: <SessionsPage /> },
-      { path: 'sessions/:id', element: <SessionPage /> },
+      { path: "sessions", element: <SessionsPage /> },
+      { path: "sessions/:id", element: <SessionPage /> },
     ],
   },
 ]);
