fbeb16ecdea4bb8d50a15e5a6ee57001eeb4d273
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-03 20:09:01 +0530

    Started building logic to render the available sessions

--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -1,6 +1,9 @@
-import { SESSIONS } from "../dummy-sessions.ts"; // normally, we would probably load that from a server
+import { useBookedSessionsContext } from "../context/BookedSessionsContext.tsx";
+import Button from "../ui/Button.tsx";
 
 export default function SessionsPage() {
+  const { sessions } = useBookedSessionsContext();
+
   return (
     <main id="sessions-page">
       <header>
@@ -11,7 +14,27 @@ export default function SessionsPage() {
           you!
         </p>
       </header>
-      {/* Todo: Output list of sessions */}
+      {sessions.length ? (
+        <ul id="sessions-list">
+          {sessions.map((session) => (
+            <li className="session-item" key={session.id}>
+              <img
+                src={session.image}
+                alt={`Illusrtation for ${session.title}`}
+              />
+              <div className="session-data">
+                <h3>{session.title}</h3>
+                <p>{session.description}</p>
+              </div>
+              <aside className="actions">
+                <Button>Learn More</Button>
+              </aside>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span>No session currently available 😔</span>
+      )}
     </main>
   );
 }
