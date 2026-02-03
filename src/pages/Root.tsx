f6a3f2db2e8cb020efeb189ce05c44425ce5470d
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-03 20:08:13 +0530

    Wrapped the app Outlet inside the booked sessions context provider component

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import BookedSessionsProvider from "../context/BookedSessionsContext";
 
 export default function Root() {
   return (
     <>
-      {/* Todo: Add Header */}
-      <Outlet />
+      <Header />
+      <BookedSessionsProvider>
+        <Outlet />
+      </BookedSessionsProvider>
     </>
   );
 }
