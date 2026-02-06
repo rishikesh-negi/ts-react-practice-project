5d8f796e7208240f9ea5981c54d11b1ce772bba6
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-06 16:43:38 +0530

    Added the logic to add a bottom border to the header anchor element that corresponds to the current URL path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../ui/Button";
 import type { ModalApi } from "./Modal";
 import Modal from "./Modal";
 import UpcomingSessions from "./UpcomingSessions";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
   const modalApiRef = useRef<ModalApi>(null);
+  const [pathname, setPathname] = useState(location.pathname);
 
   function handleOpenModal() {
     modalApiRef.current?.open();
   }
+
+  useEffect(
+    function () {
+      setPathname(location.pathname);
+    },
+    [location],
+  );
 
   return (
     <header id="main-header">
@@ -19,10 +29,16 @@ export default function Header() {
       <h1>ReactMentoring</h1>
       <nav>
         <ul>
-          <Button to="/" textOnly={true}>
+          <Button
+            to="/"
+            textOnly={true}
+            className={`${pathname === "/" ? "active" : ""}`}>
             Our Mission
           </Button>
-          <Button to="/sessions" textOnly={true}>
+          <Button
+            to="/sessions"
+            textOnly={true}
+            className={`${pathname === "/sessions" ? "active" : ""}`}>
             Browse Sessions
           </Button>
           <Button onClick={handleOpenModal}>Upcoming Sessions</Button>
